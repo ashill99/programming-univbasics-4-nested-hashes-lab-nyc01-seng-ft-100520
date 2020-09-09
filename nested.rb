327b2540ff0ee2c862d56b942eb7ec249629c7cd
Adam Shilling <admshng@gmail.com>
2020-09-09 21:35:19 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,7 +1,7 @@
 
 def hopper
 	programmer_hash = 
- 		{
+ 			{
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +55,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -68,6 +68,10 @@ def adding_matz
 
 	programmer_hash = 
  		{
+ 		  :yukihiro_matsumoto => {
+        :known_for => "Ruby",
+        :languages => ["LISP", "C"]
+      },
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -81,7 +85,6 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
 end
 
 def changing_alan
@@ -103,7 +106,9 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+      alans_new_info = "GUI"
+      programmer_hash[:alan_kay][:known_for] = alans_new_info
+      programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +130,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+programmer_hash
 end
